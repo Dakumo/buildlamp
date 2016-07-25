f42436da730fa515c881ce1e5fe2ce32e703e2d0
Torsten Zander <torsten.zander@aoe.com>
2016-07-25 17:06:51 +0200

    New Light and Monitore for teams

--- server.js
+++ server.js
@@ -13,10 +13,48 @@ var LIGHT_ID_TABLE_STEFAN = 6;
 var LIGHT_ID_TABLE_SILAS = 5;
 var LIGHT_ID_TABLE_TORSTEN = 7;
 var LIGHT_ID_PIPELINE = 8;
+var LIGHT_ID_TEAM_3 = 9;
 
-var jenkinsHueCongstar = new JenkinsHue({
+/// congo
+var jenkinsHueCongstarTeam3 = new JenkinsHue({
     jenkins: {
-        host: 'http://monitoring:andy4711congo@congstar-congo.itg.de2.dc.aoe.lan:8080/view/Congo/view/Monitoring/'
+        host: 'http://monitoring:andy4711congo@congstar-congo.itg.de2.dc.aoe.lan:8080/view/Congo%20Monitoring/view/Team%203/'
+    },
+    hue: {
+        host: '10.165.103.252',
+        username: 'newdeveloper'
+    }
+});
+var jenkinsHueCongstarTeam7 = new JenkinsHue({
+    jenkins: {
+        host: 'http://monitoring:andy4711congo@congstar-congo.itg.de2.dc.aoe.lan:8080/view/Congo%20Monitoring/view/Team%207/'
+    },
+    hue: {
+        host: '10.165.103.252',
+        username: 'newdeveloper'
+    }
+});
+var jenkinsHueCongstarTeam10 = new JenkinsHue({
+    jenkins: {
+        host: 'http://monitoring:andy4711congo@congstar-congo.itg.de2.dc.aoe.lan:8080/view/Congo%20Monitoring/view/Team%2010/'
+    },
+    hue: {
+        host: '10.165.103.252',
+        username: 'newdeveloper'
+    }
+});
+var jenkinsHueCongstarTeam12 = new JenkinsHue({
+    jenkins: {
+        host: 'http://monitoring:andy4711congo@congstar-congo.itg.de2.dc.aoe.lan:8080/view/Congo%20Monitoring/view/Team%2012/'
+    },
+    hue: {
+        host: '10.165.103.252',
+        username: 'newdeveloper'
+    }
+});
+var jenkinsHuePipeline = new JenkinsHue({
+    jenkins: {
+        host: 'http://monitoring:andy4711congo@congstar-congo.itg.de2.dc.aoe.lan:8080/view/Congo%20Monitoring/view/Pipeline/'
     },
     hue: {
         host: '10.165.103.252',
@@ -24,6 +62,7 @@ var jenkinsHueCongstar = new JenkinsHue({
     }
 });
 
+//////
 var jenkinsHueColumbus = new JenkinsHue({
     jenkins: {
         host: 'http://monitoring:andy4711congo@congstar-congo.itg.de2.dc.aoe.lan:8080/view/Cobra/view/Monitoring/'
@@ -54,24 +93,17 @@ var jenkinsHueApp = new JenkinsHue({
     }
 });
 
-var jenkinsHueAppPipeline = new JenkinsHue({
-    jenkins: {
-        host: 'http://monitoring:andy4711congo@congstar-congo.itg.de2.dc.aoe.lan:8080/view/Congo%20Monitoring/view/Pipeline/'
-    },
-    hue: {
-        host: '10.165.103.252',
-        username: 'newdeveloper'
-    }
-});
+
 
 function updateLights() {
-    jenkinsHueCongstar.setLightForJenkinsView(LIGHT_ID_TABLE_ALEX);
-    jenkinsHueCongstar.setLightForJenkinsView(LIGHT_ID_TABLE_BILAL);
-    jenkinsHueCongstar.setLightForJenkinsView(LIGHT_ID_TABLE_FLORIAN);
-    jenkinsHueCongstar.setLightForJenkinsView(LIGHT_ID_TABLE_TORSTEN);
+    jenkinsHueCongstarTeam10.setLightForJenkinsView(LIGHT_ID_TABLE_ALEX);
+    jenkinsHueCongstarTeam7.setLightForJenkinsView(LIGHT_ID_TABLE_BILAL);
+    jenkinsHueCongstarTeam7.setLightForJenkinsView(LIGHT_ID_TABLE_FLORIAN);
+    jenkinsHueCongstarTeam12.setLightForJenkinsView(LIGHT_ID_TABLE_TORSTEN);
+    jenkinsHueCongstarTeam3.setLightForJenkinsView(LIGHT_ID_TEAM_3);
     jenkinsHueCobraAw.setLightForJenkinsView(LIGHT_ID_TABLE_STEFAN);
     jenkinsHueApp.setLightForJenkinsView(LIGHT_ID_TABLE_SILAS);
-    jenkinsHueAppPipeline.setLightForJenkinsView(LIGHT_ID_PIPELINE);
+    jenkinsHuePipeline.setLightForJenkinsView(LIGHT_ID_PIPELINE);
 }
 
 function switchLightsOn() {
@@ -80,13 +112,14 @@ function switchLightsOn() {
 }
 
 function switchLightsOff() {
-    jenkinsHueCongstar.hue.switchOff(LIGHT_ID_TABLE_ALEX);
-    jenkinsHueCongstar.hue.switchOff(LIGHT_ID_TABLE_BILAL);
-    jenkinsHueCongstar.hue.switchOff(LIGHT_ID_TABLE_FLORIAN);
-    jenkinsHueCongstar.hue.switchOff(LIGHT_ID_TABLE_TORSTEN);
-    jenkinsHueCongstar.hue.switchOff(LIGHT_ID_TABLE_STEFAN);
-    jenkinsHueCongstar.hue.switchOff(LIGHT_ID_TABLE_SILAS);
-    jenkinsHueCongstar.hue.switchOff(LIGHT_ID_PIPELINE);
+    jenkinsHueCongstarTeam10.hue.switchOff(LIGHT_ID_TABLE_ALEX);
+    jenkinsHueCongstarTeam7.hue.switchOff(LIGHT_ID_TABLE_BILAL);
+    jenkinsHueCongstarTeam7.hue.switchOff(LIGHT_ID_TABLE_FLORIAN);
+    jenkinsHueCongstarTeam12.hue.switchOff(LIGHT_ID_TABLE_TORSTEN);
+    jenkinsHueCongstarTeam3.hue.switchOff(LIGHT_ID_TEAM_3);
+    jenkinsHueCobraAw.hue.switchOff(LIGHT_ID_TABLE_STEFAN);
+    jenkinsHueApp.hue.switchOff(LIGHT_ID_TABLE_SILAS);
+    jenkinsHuePipeline.hue.switchOff(LIGHT_ID_PIPELINE);
     switchedOn = false;
 }
 
